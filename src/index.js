75b3a4a5f1a25156b2cbc5f581cb031c76491f74
Sérgio Ramos <mail@sergioramos.me>
2017-09-26 20:20:00 +0100

    support for invalid values

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const flatten = require('lodash.flatten');
+const isNaN = require('lodash.isnan');
 const remcalc = require('remcalc');
 
 const BASE = 6;
 
 const calc = (base, ...values) =>
   flatten(values.map(value => String(value).split(/\s/gim)))
-    .map(value => remcalc(Number(value) * base))
+    .map(value => {
+      const _value = Number(value);
+      return isNaN(_value) ? value : remcalc(_value * base);
+    })
     .join(' ');
 
 module.exports = (...values) => calc(BASE, ...values);
